0ef54d7fd79a0e92439ccfc23bfe71cd6c4f35a7
Bruno Aggierni <bruno.aggierni@gmail.com>
2024-04-01 16:15:43 +0100

    Carousel updated

--- scripts/components/carousel.js
+++ scripts/components/carousel.js
@@ -1,36 +1,14 @@
-const images = [
-  "https://picsum.photos/1920/1080",
-  "https://picsum.photos/1921/1080",
-  "https://picsum.photos/1922/1080",
-];
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
 
-const fallbackImage = "https://via.placeholder.com/1920x1080";
-
-let currentIndex = 0;
-const carouselImage = document.getElementById("carouselImage");
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
-
-// Function to load the current image
-function loadCurrentImage() {
-  carouselImage.src = images[currentIndex];
-}
-
-// Set the initial image
-loadCurrentImage();
-
-// Add event listeners to the buttons
-prevBtn.addEventListener("click", function () {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  loadCurrentImage();
+nextButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft += slideWidth;
 });
 
-nextBtn.addEventListener("click", function () {
-  currentIndex = (currentIndex + 1) % images.length;
-  loadCurrentImage();
-});
-
-// Set fallback image if loading fails
-carouselImage.addEventListener("error", function () {
-  carouselImage.src = fallbackImage;
+prevButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slidesContainer.scrollLeft -= slideWidth;
 });
