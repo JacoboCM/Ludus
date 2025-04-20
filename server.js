ccbd6f8c5590e5f1dc22a6a70ad310ea0880a6be
Bruno Aggierni <bruno.aggierni@gmail.com>
2025-04-20 21:04:21 +0100

    Changes on server.js reverted

--- server.js
+++ server.js
@@ -13,13 +13,6 @@ const detallesJuegoRoutes = require('./routes/detalleJuego');
 // Monta las rutas
 app.use('/api/juego', detallesJuegoRoutes);
 
-// Secure endpoint to get TMDB API key
-app.get("/api/config", (req, res) => {
-  res.json({
-    tmdbApiKey: process.env.TMDB_API_KEY,
-  });
-});
-
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
